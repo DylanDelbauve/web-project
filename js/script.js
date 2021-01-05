7861a0720fbf30180b149d7f44dc7276afd2a1d7
JykaWeston <dylandelbauve@gmail.com>
2021-01-05 16:34:28 +0100

    research working

--- js/script.js
+++ js/script.js
@@ -1,38 +1,76 @@
-console.log("File loaded");
-
 function $(id) {
   return document.getElementById(id);
 }
 
 let map;
+let markers = [];
 
-  function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-      center: { lat: 47.242249900590146 , lng: 6.016386412890068 },
-      zoom: 8,
-    });
+function initMap() {
+  map = new google.maps.Map($("map"), {
+    center: { lat: 47.242249900590146, lng: 6.016386412890068 },
+    zoom: 8,
+  });
 }
 
-document.getElementById("search")
-    .onsubmit= function (event) {
-      event.preventDefault();
-      console.log("Button triggered");
-      var request = new XMLHttpRequest();
-      console.log(request.readyState);
-      request.onreadystatechange = function () {
-        
-        if (this.readyState == 4 && this.status == 200) {
-
-          alert(this.responseText)
-        }
-      };
-      request.open(
-        "GET",
-        "treatment.php?selection=" +
-          $("selection").value +
-          "&research=" +
-          $("research").value,
-        true
-      );
-      request.send();
+$("search").onsubmit = function (event) {
+  event.preventDefault();
+  resetMarkers();
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      if (this.responseText === "") {
+        alert("Aucune donnée trouvée");
+      } else {
+        var dataTemp = this.responseText.split("}");
+        var data = parseJSON(dataTemp);
+        data.forEach((e) => {
+          markers.push(createMarker(e));
+        });
+        map.setCenter({
+          lat: parseFloat(data[0].latitude),
+          lng: parseFloat(data[0].longitude),
+        });
+        map.setZoom(13);
+      }
     }
+  };
+  request.open(
+    "GET",
+    "treatment.php?selection=" +
+      $("selection").value +
+      "&research=" +
+      $("research").value,
+    true
+  );
+  request.send();
+};
+
+function createMarker(params) {
+  return new google.maps.Marker({
+    map,
+    animation: google.maps.Animation.DROP,
+    position: {
+      lat: parseFloat(params.latitude),
+      lng: parseFloat(params.longitude),
+    },
+    title: params.nom,
+  })
+}
+
+function parseJSON(params) {
+  var res = [];
+  params.forEach((e) => {
+    if (e !== "") {
+      e = e + "}";
+      res.push(JSON.parse(e));
+    }
+  });
+  return res;
+}
+
+function resetMarkers() {
+  markers.forEach((e) => {
+    e.setMap(null);
+    e = null;
+  });
+}
--- treatment.php
+++ treatment.php
@@ -5,12 +5,69 @@ $research = $_REQUEST["research"];
 
 try {
    $conn = new PDO("mysql:host=localhost;dbname=web-project", "webproject", "123456");
-
    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   $req = null;
 
-   if (strcmp($selection,"musee"){
-      
-   } )
+   if (strcmp($selection,"musee") == 0){
+      if(preg_match("/\d{5}/",$research)) {
+         $req = $conn->prepare("
+         SELECT nom, adresse, codepostal, commune, latitude, longitude, telephone, courriel, siteinternet, descriptiflong
+         FROM musees 
+         WHERE CodePostal=:CP
+         ");
+         $req->bindParam(':CP', $research);
+         $req->execute();
+         while($data = $req->fetch(PDO::FETCH_ASSOC)) {
+            $temp = hydrateMusee($data);
+            echo json_encode($temp);
+         }
+      } else if(preg_match("/[A-z]/",$research)) {
+         $req = $conn->prepare("
+         SELECT nom, adresse, codepostal, commune, latitude, longitude, telephone, courriel, siteinternet, descriptiflong
+         FROM musees 
+         WHERE commune or nom LIKE CONCAT('%',:commune,'%')
+         ");
+         $req->bindParam(':commune', $research);
+         $req->execute();
+         while($data = $req->fetch(PDO::FETCH_ASSOC)) {
+            $temp = hydrateMusee($data);
+            echo json_encode($temp);
+      }
+   } else if(strcmp($selection,"monumentshistoriques") == 0) {
+
+   }
+}
+
 } catch (PDOException $e) {
    echo "Connection failed: " . $e->getMessage();
+}
+
+function hydrateMusee($var)
+{
+   $data = array(
+      "nom" => $var['nom'],
+      "adresse" => $var['adresse'],
+      "codepostal" => $var['codepostal'],
+      "commune" => $var['commune'],
+      "latitude" => $var['latitude'],
+      "longitude" => $var['longitude'],
+      "telephone" => $var['telephone'],
+      "courriel" => $var['courriel'],
+      "siteinternet" => $var['siteinternet'],
+      "descriptiflong" => $var['descriptiflong']
+   );
+   return $data;
+}
+
+function hydrateMonument($var)
+{
+   $data = array(
+      "nom" => $var['designation'],
+      "codepostal" => $var['codepostal'],
+      "commune" => $var['departement'],
+      "latitude" => $var['latitude'],
+      "longitude" => $var['longitude'],
+      "descriptiflong" => $var['description']
+   );
+   return $data;
 }
